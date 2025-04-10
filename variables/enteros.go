ad62766450fc471ee295a2f49b45130c08d9b853
aldo <aldo_lo@outlook.com>
2025-04-09 18:31:57 -0600

    Primer Commit

---
+++ variables/enteros.go
@@ -0,0 +1,13 @@
+package variables
+
+import "fmt"
+
+func MostrarEnteros() {
+	//declaracion
+	var entero int
+	//asignacion
+	edad := 15
+
+	fmt.Println("entero = ", entero)
+	fmt.Println("edad = ", edad)
+}
