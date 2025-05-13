0ea28c5d4d6502fe02e0cff4c70921cabd8dc71a
aldo <aldo_lo@outlook.com>
2025-05-12 18:27:58 -0600

    Siguiente Commit

--- main.go
+++ main.go
@@ -4,17 +4,22 @@ import (
 	//"fmt"
 	//"godesde0/variables"
 	//"godesde0/funciones"
-	"godesde0/modelos"
-	fI "godesde0/funciones_Interfaces"
+	//"godesde0/modelos"
+	//fI "godesde0/funciones_Interfaces"
+	"fmt"
+	"godesde0/goroutines"
 )
 
 func main() {
 	/*estado, texto := variables.ConvertiraTexto(123)
 	fmt.Println(estado)
 	fmt.Println(texto)*/
-	Pedro := new(modelos.Hombre)
+	/* Pedro := new(modelos.Hombre)
 	fI.HumanoRespirando(Pedro)
 
 	Sandra := new(modelos.Mujer)
-	fI.HumanoRespirando(Sandra)
+	fI.HumanoRespirando(Sandra) */
+	go goroutines.MiNombreLento("Sergio Perez")
+
+	fmt.Println("Estoy aqui")
 }
